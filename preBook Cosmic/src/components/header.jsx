765c8b2f92b8bd20136253e7bbf74f06c1f3109a
th3m4nd4r1n <mdua_be23@thapar.edu>
2024-11-08 18:18:51 +0530

    created a portion of the landing page

---
+++ preBook Cosmic/src/components/header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <>
+        <ul className="nav">
+            
+            <li>
+                <a href="#home"><button className="menu">Home</button></a>
+
+            </li>
+            <li>
+                <a href="#features"><button className="menu">Features</button></a>
+            </li>
+            <li>
+                <a href="#reviews"><button className="menu">Reviews</button></a>
+
+            </li>
+            <li>
+                <a href="#aboutus"><button className="menu">About Us</button></a>
+            </li>
+        </ul>   
+        
+        </>
+    )
+}
+export default Header
---
+++ preBook Cosmic/src/components/hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Hero = () => {
+    return (
+        <>
+        <div className="hero">
+
+       <h1>COSMIC <br /> ATTIRE</h1>
+       <p className="desc">
+       Ut enim ad minim <br /> veniam, quis nostrud <br /> exercitation ullamco <br /> laboris nisi ut aliquip <br /> ex ea commodo <br /> consequat. 
+       </p>
+        </div>
+        </>
+    )
+}
+export default Hero
