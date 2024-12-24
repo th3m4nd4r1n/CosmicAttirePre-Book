ef2635e34ea36082fa46c2d8527281eb23db5c1b
th3m4nd4r1n <mdua_be23@thapar.edu>
2024-12-24 06:35:53 +0530

    fixed css for mobile

--- preBook Cosmic/src/components/Model.jsx
+++ preBook Cosmic/src/components/Model.jsx
@@ -7,10 +7,16 @@ import { sRGBEncoding } from "@react-three/drei/helpers/deprecated";
     // 
 
   
+function max(a,b){
+  return a>b?a:b;
+}
 const Model = () => {
-      function tick() {
-    useFrame((state, delta) => {
-       ref.current.rotation.y = 0.05*Math.cos(state.clock.elapsedTime) + 0.45;
+  
+  var scalingFactor = (max(window.innerWidth/1904, 0.5))*0.165;
+ const viewport = useThree((state) => state.viewport);
+ function tick() {
+   useFrame((state, delta) => {
+     ref.current.rotation.y = 0.05*Math.cos(state.clock.elapsedTime) + 0.45;
        refRot.current.position.x = 3*Math.sin(state.clock.elapsedTime)
        refRot2.current.position.y = -3*Math.sin(state.clock.elapsedTime)
        refRot.current.position.y = 3*Math.cos(state.clock.elapsedTime)
@@ -18,6 +24,7 @@ const Model = () => {
        refRot.current.position.z = 0.8
        ref.current.rotation.z = 0.05*Math.sin(state.clock.elapsedTime);
        ref.current.rotation.x = -0.05*Math.sin(state.clock.elapsedTime);
+       
     })}
    tick();
 
@@ -31,7 +38,7 @@ const Model = () => {
       <pointLight ref={refRot2} color={'gray'} intensity={20} />
       <Environment preset="studio" environmentIntensity={0.028} />
       <mesh ref={ref} >
-        <primitive object={scene} scale={0.15} />
+        <primitive object={scene} scale={scalingFactor} />
       </mesh>
     </>
         );
